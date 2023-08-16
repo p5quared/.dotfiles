d9102cb53d582ac6969063a9feccbd431501f898
Peter Vaiciulis <petervaiciulis@gmail.com>
2023-08-16 15:28:09 -0400

    collection of remaps for managing spaces

--- .config/nvim/after/plugin/remaps.lua
+++ .config/nvim/after/plugin/remaps.lua
@@ -1,11 +1,21 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 
+-- Move selection
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("x", "<leader>p", "\"_dP")
-
-
+-- Copilot yuk
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+
+-- Space creation
+vim.keymap.set("n", "<leader>sv", "<C-w>v<C-w>l")
+vim.keymap.set("n", "<leader>sh", "<C-w>s<C-w>j")
+
+-- Space mobility
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
