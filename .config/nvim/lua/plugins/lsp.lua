e725b01c3b3462a8fd91a0bca1f16084fe438515
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2024-06-11 09:43:03 -0400

    deprecated a

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -38,8 +38,6 @@ return {
 	init = function()
 		local lsp_zero = require('lsp-zero')
 
-		lsp_zero.preset("recommended")
-
 		lsp_zero.on_attach(function(_, bufnr)
 			lsp_zero.default_keymaps({ buffer = bufnr })
 
