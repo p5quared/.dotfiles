a779d70a46ced1463d4570156e7f1ac8ea28c58f
Peter Vaiciulis <petesv@amazon.com>
2024-10-12 15:15:10 -0700

    chore: bidirectional leap

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -20,7 +20,10 @@ return {
 			'tpope/vim-repeat'
 		},
 		init = function()
-			require('leap').create_default_mappings()
+			vim.keymap.set('n', 's', '<Plug>(leap)')
+			vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+			vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap-forward)')
+			vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
 		end,
 	},
 	{
