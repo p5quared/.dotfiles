9cd6a35c5107a2256c409dd5faa3708e340d40df
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-12-06 18:58:29 -0800

    chore: tidy colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,28 +1,10 @@
 return {
-	"oonamo/ef-themes.nvim",
 	"folke/tokyonight.nvim",
-	"RRethy/base16-nvim",
 	"catppuccin/nvim",
 	"gbprod/nord.nvim",
-	"yorik1984/newpaper.nvim",
 	'Mofiqul/vscode.nvim',
 	"vague2k/vague.nvim",
-	"yorickpeterse/vim-paper",
-	"p00f/alabaster.nvim",
-	"ashen-org/ashen.nvim",
 	{ 'datsfilipe/vesper.nvim' },
-	{
-		'jesseleite/nvim-noirbuddy',
-		dependencies = {
-			{ 'tjdevries/colorbuddy.nvim' }
-		},
-		lazy = false,
-		priority = 1000,
-		opts = {
-			preset = "slate"
-		},
-		-- install = { colorscheme = { "noirbuddy" } }
-	},
 	{
 		'ramojus/mellifluous.nvim',
 		-- opts = {
