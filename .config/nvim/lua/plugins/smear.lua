9844648a6a30e8a3706c01e1092844c0afc544b6
Peter Vaiciulis <petesv@amazon.com>
2025-12-08 01:54:03 +0000

    feat: smear

---
+++ .config/nvim/lua/plugins/smear.lua
@@ -0,0 +1,12 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+	cursor_color ="#FF8800",
+	stiffness=0.1,
+	traling_stiffness=0.1,
+	trailing_exponent=1,
+	anticipation=0.1,
+	gamma = 1,
+	hide_target_hack = true,
+  },
+}
