d879df5a9682d960a63dffab5fb37c6e80a1e6f9
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-12-15 22:30:19 -0800

    feat: colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,24 +1,5 @@
 return {
-	"folke/tokyonight.nvim",
-	"catppuccin/nvim",
-	"gbprod/nord.nvim",
 	'Mofiqul/vscode.nvim',
 	"vague2k/vague.nvim",
-	{ 'datsfilipe/vesper.nvim' },
-	{
-		'ramojus/mellifluous.nvim',
-		-- opts = {
-		-- 	styles = {
-		-- 		comments = { italic = true },
-		-- 		-- main_keywords = { bold = true },
-		-- 		-- other_keywords = { bold = true },
-		-- 	},
-		-- }
-	},
-	{
-		"mcchrish/zenbones.nvim",
-		dependencies = {
-			"rktjmp/lush.nvim"
-		},
-	},
+	"Shatur/neovim-ayu",
 }
