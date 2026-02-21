90650de6db02ede5c95de6c273accafade76f2c7
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2026-02-21 14:41:25 -0800

    feat: telescope path_display filename_first and format file

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,21 +1,30 @@
 return {
 	'nvim-telescope/telescope.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
+	opts = {
+		defaults = {
+			path_display = { "filename_first" }
+		}
+	},
 	keys = {
-		{ '<leader>ff', function() require('telescope.builtin').find_files() end, desc = "[F]ind [F]ile" },
-		{ '<leader>ft', function() require('telescope.builtin').git_files() end, desc = "[F]ind [T]ree" },
-		{ '<leader>fg', function() require('telescope.builtin').live_grep() end, desc = "[F]ind [G]rep" },
+		{ '<leader>ff', function() require('telescope.builtin').find_files() end,  desc = "[F]ind [F]ile" },
+		{ '<leader>ft', function() require('telescope.builtin').git_files() end,   desc = "[F]ind [T]ree" },
+		{ '<leader>fg', function() require('telescope.builtin').live_grep() end,   desc = "[F]ind [G]rep" },
 		{ '<leader>fc', function() require('telescope.builtin').colorscheme() end, desc = "[F]ind [C]olorscheme" },
-		{ '<leader>fk', function() require('telescope.builtin').keymaps() end, desc = "[F]ind [K]eymaps" },
-		{ '<leader>fs', function()
-			local opts = {}
-			if vim.bo.filetype == 'java' then
-				opts.filter_fn = function(symbol)
-					local uri = symbol.location and symbol.location.uri or ''
-					return not uri:match('^jdt://')
+		{ '<leader>fk', function() require('telescope.builtin').keymaps() end,     desc = "[F]ind [K]eymaps" },
+		{
+			'<leader>fs',
+			function()
+				local opts = {}
+				if vim.bo.filetype == 'java' then
+					opts.filter_fn = function(symbol)
+						local uri = symbol.location and symbol.location.uri or ''
+						return not uri:match('^jdt://')
+					end
 				end
-			end
-			require('telescope.builtin').lsp_dynamic_workspace_symbols(opts)
-		end, desc = "[F]ind [S]ymbols" },
+				require('telescope.builtin').lsp_dynamic_workspace_symbols(opts)
+			end,
+			desc = "[F]ind [S]ymbols"
+		},
 	},
 }
